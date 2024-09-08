95ddaa4d4739f802e42c6c905e9ee660abce48f6
sonnguyen123526 <nguyenso@dickinson.edu>
2024-09-07 21:14:15 -0400

    HW0 submission

---
+++ src/hw0/SlimeSpaceAlien.java
@@ -0,0 +1,35 @@
+package hw0;
+
+public class SlimeSpaceAlien extends SpaceAlien implements ZapsWithSlime{
+    
+    private int age;
+
+    public SlimeSpaceAlien(String homePlanet, double distanceFromEarth, int age) {
+        super(homePlanet, distanceFromEarth);
+        this.age = age;
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String doSlimeZap(int numHumans) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doSlimeZap'");
+    }
+
+    @Override
+    public String slimeWholeEarth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'slimeWholeEarth'");
+    }
+
+    @Override
+    public void doGreeting(){
+        System.out.println("Greetings form planet " + gethomePlanet() + ", "
+                + getdistanceFromEarth() + " light years away with the age of" + " "+ age);
+    }
+
+    public static void main(String[] args){
+        SlimeSpaceAlien s1 = new SlimeSpaceAlien("Earth", 1000, 10000);
+        s1.doGreeting();
+    }
+}
