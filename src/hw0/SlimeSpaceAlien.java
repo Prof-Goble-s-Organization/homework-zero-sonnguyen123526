43236a5ea85a17d982685579819cbafceaa9a7af
sonnguyen123526 <nguyenso@dickinson.edu>
2024-09-10 15:13:37 -0400

    Please Work

--- src/hw0/SlimeSpaceAlien.java
+++ src/hw0/SlimeSpaceAlien.java
@@ -24,7 +24,7 @@ public class SlimeSpaceAlien extends SpaceAlien implements ZapsWithSlime{
 
     @Override
     public void doGreeting(){
-        System.out.println("Hey form planet " + gethomePlanet() + ", "
+        System.out.println("Hello form planet " + gethomePlanet() + ", "
                 + getdistanceFromEarth() + " light years away with the age of" + " "+ age);
     }
 
