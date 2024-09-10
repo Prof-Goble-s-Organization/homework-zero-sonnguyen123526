0c09c9469eb28f89ea4c7a499c9dc415a5770cb2
sonnguyen123526 <nguyenso@dickinson.edu>
2024-09-10 15:34:51 -0400

    Commit testing hopefully fine

--- src/hw0/SpaceAlien.java
+++ src/hw0/SpaceAlien.java
@@ -22,7 +22,7 @@ public class SpaceAlien {
     }
 
     public void doGreeting() {
-        System.out.println("Greetings form planet" + homePlanet + ", "
+        System.out.println("Greetings form planet " + homePlanet + ", "
                 + distanceFromEarth + " light years away");
     }
 
